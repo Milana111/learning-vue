691c50e8787b795a9d09594a1c7374b827681789
Milana111 <milananuriakhmetova@gmail.com>
2020-11-10 22:08:02 +0600

    Edited vue-cli project

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="wrapper">
+
+<div class="wrapper-content">
+  <section>
+    <div class="container">
+      <h1> HEIIO </h1>
+ <img alt="Vue logo" src="./assets/logo.png">
+    </div>
+  </section>
   </div>
+
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
+export default {}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ vue-cli/src/vue.config.js
@@ -0,0 +1,10 @@
+// vue.config.js
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/',
+    assetsDir: '',
+    productionSourceMap:false,
+    filenameHashing: false,
+
+  }
